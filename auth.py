0904e3cdc10196b041f3dddb0c9767e33f5257f3
onurbsofa <brunofazio466@gmail.com>
2024-09-10 23:14:16 -0300

    loop login

--- auth.py
+++ auth.py
@@ -1,15 +1,25 @@
-def registrar_usuario(matriz_usuarios, username, password):
-    for usuario in matriz_usuarios:
-        if usuario[0] == username:
-            print("El nombre de usuario ya existe.")
-            return False
-    matriz_usuarios.append([username, password])
+usuarios = ["usuario1", "usuario2", "usuario3"]
+contrasenas = ["password1", "password2", "password3"]
+
+def menu_login():
+    print("1. Iniciar sesión.")
+    print("2. Registrarse.")
+    print("3. Salir.")
+    opcion = int(input("Ingrese el numero de la opcion: "))
+    return opcion
+
+def registrar_usuario(usuarios, contrasenas, username, password):
+    if username in usuarios:
+        print("El nombre de usuario ya existe.")
+        return False
+    usuarios.append(username)
+    contrasenas.append(password)
     print("Usuario registrado exitosamente.")
     return True
 
-def verificar_credenciales(matriz_usuarios, username, password):
-    for usuario in matriz_usuarios:
-        if usuario[0] == username and usuario[1] == password:
+def verificar_credenciales(usuarios, contrasenas, username, password):
+    for i in range(len(usuarios)):
+        if usuarios[i] == username and contrasenas[i] == password:
             print("Inicio de sesión exitoso.")
             return True
     print("Nombre de usuario o contraseña incorrectos.")
--- main.py
+++ main.py
@@ -1,42 +1,49 @@
 from tickets.ticket import validar_empleados, generar_ids
 from menu.menu import menu
-from auth import registrar_usuario, verificar_credenciales
-
+from auth import registrar_usuario, verificar_credenciales, menu_login
 
 def main():
+    # Listas paralelas para usuario y contraseñas
+    usuarios = ["usuario1", "password1"]
+    contrasenas = ["usuario2", "password2"]
 
-    usuarios = [
-        ["usuario1", "password1"],
-        ["usuario2", "password2"],
-        ["usuario3", "password3"],
-    ]
 
-    # Ingreso de usuario y contraseña
-    sesion_actual = verificar_credenciales(usuarios, "usuario1", "password1")
-    if not sesion_actual:
-        print("Inicio de sesión fallido.")
-        return
+    continuar = True
 
-    # Registro de usuario
-    """ registro_exitoso = registrar_usuario(usuarios, "usuario4", "password4") """
+    while continuar:
+        opcion = menu_login()
+        
+        if opcion == 1:
+            username = input("Ingrese nombre de usuario: ")
+            password = input("Ingrese contraseña: ")
+            if verificar_credenciales(usuarios, contrasenas, username, password):
+                print("Acceso concedido.")
+                total_empleados = validar_empleados()
+                filas = total_empleados  # Cantidad de empleados
+                columnas = 12  # 12 una por cada mes
 
-    total_empleados = validar_empleados()
-    filas = total_empleados  # Cantidad de empleados
-    columnas = 12  # 12 una por cada mes
+                matriz_empleados = []  # lista para inicializar la matriz
+                ids = []  # lista de ids para empleados
 
-    matriz_empleados = []  # lista para inicializar la matriz
-    ids = []  # lista de ids para empleados
+                for f in range(filas):
+                    matriz_empleados.append([0] * columnas)  # creacion de matriz de forma dinamica
 
-    for f in range(filas):
-        matriz_empleados.append([0] * columnas)  # creacion de matriz de forma dinamica
+                if not ids:  # generacion de ids automaticamente post ingresar numero de empleados
+                    generar_ids(len(matriz_empleados), ids)
 
-    if not ids:  # generacion de ids automaticamente post ingresar numero de empleados
-        generar_ids(len(matriz_empleados), ids)
-
-    """Menú principal para la gestión de tickets."""
-
-    menu(matriz_empleados, columnas, ids)
-
+                """Menú principal para la gestión de tickets."""
+                menu(matriz_empleados, columnas, ids)
+            else:
+                print("Acceso denegado.")
+        elif opcion == 2:
+            username = input("Ingrese nombre de usuario: ")
+            password = input("Ingrese contraseña: ")
+            registrar_usuario(usuarios, contrasenas, username, password)
+        elif opcion == 3:
+            print("Saliendo del sistema.")
+            continuar = False
+        else:
+            print("Opción no válida. Intente de nuevo.")
 
 if __name__ == "__main__":
     main()
