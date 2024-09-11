04e265c38034443177f292b38ab17155269a173e
Julian Dos Santos <dss.julian@gmail.com>
2024-09-10 23:03:33 -0300

    Fix para error al actualizar un valor de la matriz

--- tickets/ticket.py
+++ tickets/ticket.py
@@ -16,33 +16,47 @@ def generar_ids(total_empleados, ids):
         if id not in ids:
             ids.append(id)
             print("Empleado", id)
-    print("\n --- IDs generados:", ids,"---")
+    print("\n --- IDs generados:", ids, "---")
 
 
 def cargar_ticket_manual(matriz, ids):
 
     salir = False
-    print(f'\n --- IDs de los empleados: {ids} ---')
+    print(f"\n --- IDs de los empleados: {ids} ---")
 
     while not salir:
 
-        empleado = int(input("Ingrese el ID del empleado al que desea modificarle un ticket: "))
-        while empleado not in ids <= 0:
-            empleado = int(input("Error - ID de empleado inválido. Intente con un ID existente: "))
+        empleado = int(
+            input("Ingrese el ID del empleado al que desea modificarle un ticket: ")
+        )
+        while empleado not in ids:
+            empleado = int(
+                input("Error - ID de empleado inválido. Intente con un ID existente: ")
+            )
 
         mes = int(input("Ingrese el mes (1-12) para el cual desea cargar el valor: "))
         while mes < 1 or mes > 12:
-            mes = int(input("Error - Mes inválido. Debe ingresar un valor entre 1 y 12: "))
+            mes = int(
+                input("Error - Mes inválido. Debe ingresar un valor entre 1 y 12: ")
+            )
 
         cantidad = int(input("Ingrese la cantidad total de tickets: "))
         while cantidad <= 0 and cantidad <= 100:
-            cantidad = int(input("Error - Cantidad inválida. Ingrese un valor mayor a 0, y menor, o igual a 100"))
+            cantidad = int(
+                input(
+                    "Error - Cantidad inválida. Ingrese un valor mayor a 0, y menor, o igual a 100"
+                )
+            )
 
         matriz[ids.index(empleado)][
             mes - 1
         ] = cantidad  # mes -1 para ir al mes ingresado por el usuario por q la lista empieza de 0 sino da mal
-        
-        salida = int(input("Ingrese 1 para continuar con otra carga, o ingrese cualquier otro número para regresar al menú: "))
+
+        salida = int(
+            input(
+                "Ingrese 1 para continuar con otra carga, o ingrese cualquier otro número para regresar al menú: "
+            )
+        )
 
         if salida != 1:
             salir = True
